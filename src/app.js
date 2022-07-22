cf93b573a0642be4f98630337fa30255f425738d
Vynnukh <bwhittingham213@gmail.com>
2022-07-22 14:57:01 +0100

    3rd Commit - fixed Delete

--- src/app.js
+++ src/app.js
@@ -12,12 +12,11 @@ const app = async (yargsObj) => {
         console.log(await readGames())
         // This will handle listing the games that are within the databse in the format of a table
     } else if (yargsObj.update) {
-        await updateSingle({title: yargsObj.updateGame}, {title: yargsObj.title, genre: yargsObj.genre})
-        console.log(await readGames())
+        await updateSingle({title: yargsObj.update}, {title: yargsObj.title, genre: yargsObj.genre})
+        console.log(updateSingle())
         // This will handle updating a specific entry from the database
     } else if (yargsObj.delete) {
         await deleteSingle({title: yargsObj.delete})
-        console.log(readGames())
         // This will handle deleting a specific entry from the database
     } else {
         console.log("Invalid Input")
